db62b537684207a0488f4aa7ad23b4a40c484c6e
Louise Whittle <louisewhittle@Louises-MacBook-Pro.local>
2018-03-07 16:12:02 +0000

    commit work in progress - step three

--- lib/rubytest.rb
+++ lib/rubytest.rb
@@ -1,7 +1,9 @@
 class RubyTest
 
   def job_order(job)
-    job
+    grouped_jobs = job.group_by {|key, value| value}
+    grouped_jobs.flatten.flatten.uniq.compact
   end
+
 
 end
--- spec/rubytest_spec.rb
+++ spec/rubytest_spec.rb
@@ -7,18 +7,33 @@ let(:jobs) {RubyTest.new}
   describe 'job_order' do
 
     it 'returns an empty string when passed no jobs' do
-      expected = ""
-      expect(jobs.job_order("")).to eq(expected)
+      expected = [""]
+      expect(jobs.job_order([""])).to eq(expected)
     end
 
     it 'returns a string when passed a job' do
-      expected = "a"
-      expect(jobs.job_order("a")).to eq(expected)
+      expected = ["a"]
+      expect(jobs.job_order(["a"])).to eq(expected)
     end
 
     it 'returns a string when passed multiple jobs' do
-      expected = "abc"
-      expect(jobs.job_order("abc")).to eq(expected)
+      expected = ["a","b","c"]
+      expect(jobs.job_order([["a"], ["b"], ["c"]])).to eq(expected)
+    end
+
+    it 'returns an ordered list of jobs with one dependency' do
+      expected = ["a","c","b"]
+      expect(jobs.job_order([["a"], ["b", "c"], ["c"]])).to eq(expected)
+    end
+
+    it 'returns an ordered list of jobs with multiple dependencies' do
+      expected = ["a", "f", "c", "b", "d", "e"]
+      expect(jobs.job_order([["a"], ["b", "c"], ["c", "f"], ["d", "a"], ["e", "b"], ["f"]])).to eq(expected)
+    end
+
+    it 'returns an ordered list of jobs with multiple dependencies - whole alphabet' do
+      expected = ["a", "f", "c", "b", "d", "e"]
+      expect(jobs.job_order([["a"],["b","c"],["c", "a"],["d"],["e","f"],["f"],["g","h"],["h"],["i"]])).to eq(expected)
     end
 
   end
