ab2fa6e3ab3d548a1c0b8e0c5b64c1e4ddd91318
Louise Whittle <louisewhittle@Louises-MacBook-Pro.local>
2018-03-13 16:21:55 +0000

    added circular check and part testing

--- lib/rubytest.rb
+++ lib/rubytest.rb
@@ -2,6 +2,12 @@ class InvalidJobs < StandardError; end
 
 class RubyTest
 
+  def sequence(grouped_jobs)
+    dependency_check(jobs)
+    error_check(jobs)
+    job_order(jobs_hash)
+  end
+
   def parse(jobs)
     jobs_hash = Hash[jobs.split(", ").map{|x| x.split(/\s\=\>\s?/)}]
   end
@@ -14,9 +20,44 @@ class RubyTest
   def dependency_check(jobs)
     dedupe_jobs = parse(jobs).each do |key, value|
       if key == value
-        raise InvalidJobs
+        raise InvalidJobs, "A job cannot refer to itself!"
       end
     end
   end
 
+  #def compact_hash(jobs_hash)
+  #end
+
+  def circular_check(jobs)
+    errors = []
+    compact_hash = parse(jobs).compact
+    compact_hash.each do |_, v|
+      keys = compact_hash.keys
+      if keys.include?(v)
+        key_pair = compact_hash.select { |k,value| k == v }.to_a.flatten
+        errors << key_pair unless errors.include?(key_pair)
+      end
+    end
+    errors.flatten
+  end
+
+  def error_count(errors)
+    errors_to_check = circular_check(errors)
+    count = 0
+    errors_to_check.each_with_index do |error, index|
+       next_error = errors[index+1]
+       previous_error = errors[index-1]
+         if error == previous_error
+          count += 1
+         end
+       end
+       count
+     end
+
+    def error_check(count)
+     if count > 0
+       raise InvalidJobs, "Job lists cannot contain circular references"
+     end
+    end
+    
 end
--- spec/rubytest_spec.rb
+++ spec/rubytest_spec.rb
@@ -31,10 +31,14 @@ let(:jobs) {RubyTest.new}
       expect(jobs.job_order("a => , b => c, c => f, d => a, e => b, f => ")).to eq(expected)
     end
 
-    # it 'returns an ordered list of jobs with multiple dependencies - whole alphabet' do
-    #   expected = ["a", "f", "i", "m", "n", "c", "b", "d", "e", "k", "j", "h", "g", "l"]
-    #   expect(jobs.job_order("a => , b => c, c => f, d => a, e => b, f => , g => h, h => j, i => , j => k, k => m, l => n, m =>, n => ")).to eq(expected)
-    # end
+    it 'returns an ordered list of jobs with multiple dependencies - whole alphabet' do
+      expected = ["a", "f", "i", "m", "n", "c", "b", "d", "e", "k", "j", "h", "g", "l"]
+      expect(jobs.job_order("a => , b => c, c => f, d => a, e => b, f => , g => h, h => j, i => , j => k, k => m, l => n, m =>, n => ")).to eq(expected)
+    end
+
+  end
+
+  describe 'dependency_check' do
 
     it 'raises an error when a job tries to depend on itself' do
       expect{ jobs.dependency_check("a => , b => , c => c") }.to raise_error(InvalidJobs)
@@ -45,4 +49,66 @@ let(:jobs) {RubyTest.new}
     end
 
   end
+
+  describe 'circular_check' do
+
+    it 'returns an array of key/value pairs' do
+      expected = ["c", "f", "f", "b", "b", "c"]
+      expect(jobs.circular_check("a => , b => c, c => f, d => a, e => , f => b")).to eq(expected)
+    end
+
+    it 'returns an empty array if there are no dependencies' do
+      expected = []
+      expect(jobs.circular_check("a => , b => , c => ")).to eq(expected)
+    end
+
+    it 'returns an empty array if there are dependencies that do not have keys in the job list' do
+      expected = []
+      expect(jobs.circular_check("a => g, b => f, c => x")).to eq(expected)
+    end
+
+  end
+
+  describe 'error_count' do
+
+    it 'returns an integer when has looping jobs' do
+      expected = 3
+      expect(jobs.error_count("a => , b => c, c => f, d => a, e => , f => b")).to eq(expected)
+    end
+
+    it 'does not return an integer when it doesnt have looping jobs' do
+      expected =
+      expect(jobs.error_count("a => , b => c, c => f, d => a, e => , f => b")).to eq(expected)
+    end
+
+  end
+
+  describe 'error_check' do
+
+  end
+
+  describe 'parse' do
+
+    it 'returns an empty string when passed no jobs' do
+      expected = {}
+      expect(jobs.parse("")).to eq(expected)
+    end
+
+    it 'returns a string when passed a job' do
+      expected = {"a" => nil}
+      expect(jobs.parse("a => ")).to eq(expected)
+    end
+
+    it 'returns a string when passed multiple jobs' do
+      expected = {"a"=>nil, "b"=>nil, "c"=>nil}
+      expect(jobs.parse("a => , b => , c => ")).to eq(expected)
+    end
+
+    it 'returns an ordered list of jobs with multiple dependencies' do
+      expected = {"a"=>nil, "b"=>"c", "c"=>"f", "d"=>"a", "e"=>"b", "f"=>nil}
+      expect(jobs.parse("a => , b => c, c => f, d => a, e => b, f => ")).to eq(expected)
+    end
+
+  end
+
 end
